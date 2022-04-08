73047977325f3d08f9706a03d0e6a300ebf7df1c
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 14:17:48 -0700

    Task 5, print signle digit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int single_digit;
 
-	for(single_digit = 0; single_digit < 0; single_digit++)
+	for (single_digit = 0; single_digit < 10; single_digit++)
 		printf("%i", single_digit);
 		putchar('\n');
 		return (0);
