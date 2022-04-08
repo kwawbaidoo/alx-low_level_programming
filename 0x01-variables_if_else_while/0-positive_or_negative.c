322713275814dddb4a6259c6fea05818d758851a
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 08:34:15 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
+
 int n;
 
 srand(time(0));
@@ -17,15 +18,18 @@ n = rand() - RAND_MAX / 2;
 	
 if (n == 0)
 {
-printf("%d is zero\n", n;
+printf("%d is zero\n", n);
 }
+
 else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
+
 else
 {
 printf("%d is positive\n", n);
 }
+
 return (0);
 }	
