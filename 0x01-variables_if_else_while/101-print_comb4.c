a3fc002cb7b1998b55f3b90f592bfb81e0405089
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 16:12:38 -0700

    Print possible combination of three digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+	 #include <stdio.h>
+
+	/**
+     	* main - print all possible combinations of three digit
+	*
+	* Return: 0
+	*/
+     int main(void)
+	{
+	  	int first_digit;
+		int second_digit;
+
+	for (first_digit = 0; first_digit <= 8; first_digit++)
+	    {
+		for (second_digit = first_digit + 1; second_digit <= 9;
+		 	second_digit++)
+		   {
+	 		for (third_digit = second_digit + 1; third_digit <= 				9; third_digit++)
+			{
+			    putchar(first_digit + '0');
+ 			    putchar(second_digit + '0');
+			    putchar(third_digit + '0');
+			}
+
+			if (first_digit < 8)
+			{
+			    putchar(',');
+			    putchar(' ');
+			}
+ 		    }
+ 	      }
+		putchar('\n');
+		
+		return (0);
+	 }
+
