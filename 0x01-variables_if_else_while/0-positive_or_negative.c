64d0f2b4d832464588297c5cb830a9e175a38ea8
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 06:23:11 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-/**
+,/**
  * main - Entry point
  * 
  * Return: Always 0
@@ -13,7 +13,7 @@ n = rand() - RAND_MAX / 2;
 	
 if (n == 0)
 {
-printf("%i is zero\n".n;
+printf("%i is zero\n", n;
 }
 else if (n < 0)
 {
