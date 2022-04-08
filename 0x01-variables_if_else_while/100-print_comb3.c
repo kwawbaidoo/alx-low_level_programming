cf6ebb3f35f093b59d0d15095300a87dbb90885c
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 15:34:23 -0700

    Print all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,22 @@
 int main(void)
 {
 	int first_digit;
-	int second-digit;
+	int second_digit;
 
-	for (first_digit = 0;first_digit <= 9; first_digit++)
+	for (first_digit = 0; first_digit <= 9; first_digit++)
 	{
 		for (second_digit = 0; second_digit <= 9; second_digit++)
 		{
 			putchar(first_digit + '0');
 			putchar(second_digit + '0');
 
-		if(!(first_digit == 9 && second-digit == 9)
+		if(!(first_digit == 9 && second_digit == 9))
 		{
 			putchar(',');
 			putchar(' ');
 		}
 
-		}
+		}	
 	}
 	putchar('\n');
 
