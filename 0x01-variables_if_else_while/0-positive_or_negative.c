bac67e053f0babb717fb8d55c591cd3a78c7ffd3
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 08:37:25 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
- * 
+ * main - Entry point 
  * Return: Always 0
  */
 
@@ -15,7 +14,6 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-	
 if (n == 0)
 {
 printf("%d is zero\n", n);
@@ -32,4 +30,5 @@ printf("%d is positive\n", n);
 }
 
 return (0);
+
 }	
