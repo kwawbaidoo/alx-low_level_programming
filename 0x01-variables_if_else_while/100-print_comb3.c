bc23a62213a5020ae2d8ee37a89702f3e2574d5c
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 15:41:33 -0700

    Print all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print all possible combinations of two digit
+ * main - print all possible combinations of two digit 00 to 89
  *
  * Return: 0
  */
@@ -10,12 +10,12 @@ int main(void)
 	int first_digit;
 	int second_digit;
 
-	for (first_digit = 0; first_digit <= 9; first_digit++)
+	for (first_digit = 0; first_digit <= 8; first_digit++)
 	{
-		for (second_digit = 0; second_digit <= 9; second_digit++)
+		for (second_digit = 1; second_digit <= 9; second_digit++)
 		{
 			putchar(first_digit + '0');
-			putchar(second_digit + '0');
+			putchar(second_digit + '1');
 
 		if(!(first_digit == 9 && second_digit == 9))
 		{
