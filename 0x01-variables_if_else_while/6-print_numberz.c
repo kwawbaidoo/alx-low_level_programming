a3beaf7067d1deb70157e137ae00b23098611dba
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 14:29:06 -0700

    Task 6, print digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit, '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
