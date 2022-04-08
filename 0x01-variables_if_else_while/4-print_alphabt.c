b912a46aa7d99f3928ef9b06d69430815fbcea21
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 13:51:43 -0700

    Task 4 print lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower and uppercase
+ * 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (ch ='A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
