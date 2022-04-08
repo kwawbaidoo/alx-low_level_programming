045eb3bc79d3d2887e8ceac63db28309ad68e0f7
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-08 14:15:03 -0700

    Task 5, print signle digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print all single numbers
+ * of base 10
+ *
+ * Return 0
+ */
+int main(void)
+{
+	int single_digit;
+
+	for(single_digit = 0; single_digit < 0; single_digit++)
+		printf("%i", single_digit);
+		putchar('\n');
+		return (0);
+}
+
