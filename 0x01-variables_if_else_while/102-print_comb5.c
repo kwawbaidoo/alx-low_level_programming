02c751e729507c3f1a01ab7480c32158e39a1e01
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-11 09:36:53 -0700

    Task 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,25 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = 0; tens <= 9; tens++)
+	for (tens = 0; tens <= 98; tens++)
 	{
-	for (ones = 0; ones <= 9; ones++)
-	{
-		putchar(tens + '0');
-		putchar(ones + '0');
-
-		if (!(tens == 9 && ones == 9))
+		for (ones = tens +1; ones <= 99; ones++)
 		{
-		putchar(',');
-		putchar(' ');
+			putchar((tens /10) + '0');
+			putchar((tens % 10) + '0');
+			putchar(' ');
+			putchar((ones / 10) + '0');
+			putchar((ones % 10) + '0');
+
+			if (tens == 98 && ones == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	}
+
+	putchar('\n');
+
 	return (0);
 }
