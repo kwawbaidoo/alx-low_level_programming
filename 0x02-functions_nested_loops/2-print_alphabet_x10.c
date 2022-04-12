c1f5132f89c62d0b8db6c0487e1d671fda7c8393
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-12 10:52:54 -0700

    Prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int count;
+
+	count = 0;
+	while (count < 0)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		count++
+		_piutchar('\n');
+	}
+}
+
