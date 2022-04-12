fc496410b4f2ab75866c976f97f0fa8c30c7930b
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-12 13:05:17 -0700

    function that prints all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @a : number to start from n
+ * Return: 0 or 1
+ */
+
+void print_to_98(int a)
+{
+	while (a < 98)
+	{
+		printf("%i, ", a);
+		a++;
+	}
+	while (a > 98)
+	{
+		printf("%i, ", a);
+		a--;
+	}
+	printf("98");
+	printf('\n');
+}
