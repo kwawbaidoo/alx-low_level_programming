91926d0bbdd93989bc006c919319d3ab3ace2c74
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-12 14:40:55 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: computes and print even number < 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, tSum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			tSum = tSum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+	printf("%ld\n", tSum);
+	return (0);
+}
