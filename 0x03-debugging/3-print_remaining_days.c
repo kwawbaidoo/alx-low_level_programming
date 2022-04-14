8e0320123e08b7a65a3260ae1dadb104e4662e90
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-13 22:57:44 -0700

    Converts a day to day of year and determins how many days left

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,6 +11,7 @@
  */
 
 void print_remaining_days(int month, int day, int year)
+
 {
 	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 	{
@@ -29,7 +30,7 @@ void print_remaining_days(int month, int day, int year)
 
 		if (month == 2  && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day                         - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 
 		else
