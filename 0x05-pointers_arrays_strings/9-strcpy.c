4ffd4dbafe10499ef6e2ecffd9682c2a530c9282
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-20 07:44:36 -0700

    Function that copies string pointe by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - copy string with terminator
+ * @dest: value to be evaluate
+ * @src: value to beevaluated
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int a = 0;
+
+	while (*(src + i) != '\n')
+	{
+		i++;
+	}
+	for ( ; x < i; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[i] = '\n';
+	return (dest);
+}
