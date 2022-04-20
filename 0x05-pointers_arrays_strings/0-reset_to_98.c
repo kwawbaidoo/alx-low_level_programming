0ac81b19f9eece078e12237177b331c39ff317c0
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-20 04:25:59 -0700

    Task o

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value it point to 98 int
+ * @n: point to
+ * Return: (0)
+ */
+void reset_to_98(int *n)
+{
+	int n;
+
+	*n = 98;
+	return (0);
+}
