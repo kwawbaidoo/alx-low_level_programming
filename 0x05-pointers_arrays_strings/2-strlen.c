d379774433486e78f36496f861fcd60a3377d695
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-20 04:52:57 -0700

    Function to evealuate the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: value to return it length
+ * Return: not
+ */
+ int _strlen(char *s)
+{
+	int strlength = 0;
+
+	strlength = *s;
+
+	_putchar("Length of *s:  %d \n", strlength);
+	return (Strlength);
+}
