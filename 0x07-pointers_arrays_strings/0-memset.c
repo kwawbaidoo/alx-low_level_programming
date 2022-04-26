b311a62cbeeb089e103592d77ca7d33f982f8b8e
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-04-26 13:37:07 -0700

    Function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: input pointer
+ * @b: input variable
+ * @n: unsigned int variable
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
