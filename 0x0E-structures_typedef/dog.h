b245e0ec38e78f6af24c57f137a36f56a288c3f5
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-11 16:14:45 -0700

    Task 0 - Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: owner of the dog
  * Return: no return
  **/
-void init_dog(struct dog * d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
