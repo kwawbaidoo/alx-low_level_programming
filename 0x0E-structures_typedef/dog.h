72b17165a9b007cd9b6e773a35dc627947c44167
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-11 16:24:08 -0700

    Task 0 - Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,7 @@
 #include "dog.h"
-
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: element of type dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
