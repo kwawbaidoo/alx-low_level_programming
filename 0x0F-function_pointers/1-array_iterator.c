d90add91efee01b5180d856a46c869f2d340ccd6
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-13 14:16:34 -0700

    function that searches for an integer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function to execute a function of array
+ * @array: pointer to array
+ * @size: number of elements in array
+ * @action: pointer to function
+ * Return: nothing
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int elem;
+
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (elem = 0; elem <size; elem++)
+			action(array[elem]);
+	}
+}
