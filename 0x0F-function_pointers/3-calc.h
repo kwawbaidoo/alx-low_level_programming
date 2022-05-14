b16838a2126527e8db9f019a5d777bef67451c92
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-14 09:45:36 -0700

    Function Pionters

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,21 +3,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <String.h>
+#include <string.h>
+
 /**
- * struct op - struct op
- * @op: the operator
- * @f: the function associated
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
