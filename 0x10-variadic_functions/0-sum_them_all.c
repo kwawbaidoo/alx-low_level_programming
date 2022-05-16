7812eac29100bf165249e2d8ab2d3774250805b5
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-16 16:24:16 -0700

    Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,9 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - function that sum of all its parameters
+ *
  * @n: How many variables will be inserted in the function
+ *
  * Return: return the sum of all its parameters
  */
 int sum_them_all(const unsigned int n, ...)
