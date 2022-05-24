862d45597bb559d56a1014e343fa20ec4b29d8ea
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-24 16:13:48 -0700

    Adding node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,13 @@
 #include "lists.h"
-
 /**
  * add_node - function that adds a new node
  * at the beginning of a list_t list.
+ *
  * @head: Parameter with the head node
  * @str: parameter with the element str in
  * the structure
- * Return: the head of the linked list
+ *
+ * Return: Return the head of the linked list
  */
 list_t *add_node(list_t **head, const char *str)
 {
