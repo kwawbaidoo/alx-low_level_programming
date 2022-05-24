20bf0c30f06d65a5f7e616782d9e085c97e3a12e
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-24 15:39:19 -0700

    Prototyping

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
