3e099fa2c1b5175561172e670d89b3aff7de1fb7
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-24 15:47:25 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list - function that priints all the elements of a list_t list
+ * print_list - function that prints all the elements of a list_t list
  * @h: Parameterwith the list
  * Return: the number of nodes
  */
