45e95e88348e0379a4d93e5c52941ebde8bc6fd4
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-24 15:44:54 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			_putchar("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			_putchar("[%u] %s\n", h->len, h->str);
 		}
 		h = h->nest;
 	}
