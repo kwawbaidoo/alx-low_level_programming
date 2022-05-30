e8bedc739a57375f8a1402a29e934050aea4507e
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-05-30 10:22:02 -0700

    File io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,51 +1,33 @@
 #include "main.h"
-/**
- * _strlen - funtion count len of char.
- * @s: count string.
- * Return: string.
- */
-int _strlen(char *s)
-{
-	int len = 0;
 
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
 /**
- * append_text_to_file - write after the file
- * @filename: name of file.
- * @text_content: text content.
- * Return: 1 ok, -1 error.
+ * append_text_to_file - appends text to file
+ * @filename: file name
+ * @text_content: content to be appended to text
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int file, len, x;
+	int fd;
+	ssize_t n;
 
-if (!filename)
-return (-1);
-file = open(filename, O_WRONLY | O_APPEND);
-if (file == -1)
-{
-return (-1);
-}
-if (!text_content)
-{
-if (close(file) == -1)
-{
-return (-1);
-}
-else
-{
-return (1);
-}
-}
-len = _strlen(text_content);
-x = write(file, text_content, len);
-if (x == -1)
-{
-return (-1);
-}
-close(file);
-return (1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_RDONLY, 0600);
+		if (fd != -1)
+			return (1);
+		else
+			return (-1);
+	}
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	n = write(fd, text_content, strlen(text_content));
+	if (n == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
